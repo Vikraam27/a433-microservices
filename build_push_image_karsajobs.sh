aa9175decb7f26d0f1dd36ccea580bdee30c41a1
Vikram <49831979+Vikraam27@users.noreply.github.com>
2023-01-20 15:42:37 +0700

    Update build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -12,7 +12,7 @@ docker images
 echo "Tag item-app image to ghcr.io/vikraam27/karsajobs:latest"
 docker tag karsajobs:latest ghcr.io/vikraam27/karsajobs:latest
 
-export GITHUB_PWD=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
+export GITHUB_PWD=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
 
 # login to github
 echo "Login to github"
